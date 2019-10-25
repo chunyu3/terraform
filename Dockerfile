315bdb3757bb4ec92a178841badbab1ab12362ac
Ubuntu <chunyu@terraform-build.3jgs2iddu3ruvn4tnxanddtbjd.ax.internal.cloudapp.net>
2019-10-25 06:17:31 +0000

    docker image

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,9 @@ MAINTAINER zikalino
 
 #ADD . /autorest.cli
 # RUN cd /autorest.cli; npm install
+ADD autorest.cli /autorest.cli
+RUN cd /autorest.cli; npm install
+ADD magic-modules /magic-modules
+RUN bash -l -c 'cd /magic-modules;gem install bundler:2.0.1; bundle update --bundler; bundle'
+ADD terraform/run.sh /run.sh
 ENTRYPOINT ["bash"]
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 cp -rf --verbose /source/dist /autorest.cli
 cp -rf --verbose /source/input /autorest.cli
-for d in $1; do
+#cd /magic-modules; bundle update --bundler; bundle
+for d in $1/*.md; do
   autorest --cli --use=/autorest.cli --mm --intermediate --python-sdks-folder=/generated --output-folder=/generated $d
-  cd /magic-modules; compiler.rb -e terraform -c azure -p /generated/magic-modules-input/alertsmanagementsmartdetectoralertrule -o /mmoutput
+  cd /magic-modules; bundle exec compiler.rb -e terraform -c azure -p /generated/alertsmanagementsmartdetectoralertrule -o /mmoutput
+  break
 done
