fbcf265a22b7c1ba088cde7ecf9bd883e3447fe0
Ubuntu <chunyu@terraform-build.3jgs2iddu3ruvn4tnxanddtbjd.ax.internal.cloudapp.net>
2019-10-25 07:10:26 +0000

    edit run script

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
 cp -rf --verbose /source/dist /autorest.cli
 cp -rf --verbose /source/input /autorest.cli
 #cd /magic-modules; bundle update --bundler; bundle
 for d in $1/*.md; do
   autorest --cli --use=/autorest.cli --mm --intermediate --python-sdks-folder=/generated --output-folder=/generated $d
-  cd /magic-modules; bundle exec compiler.rb -e terraform -c azure -p /generated/alertsmanagementsmartdetectoralertrule -o /mmoutput
-  break
+done
+src="/generated"
+if [ ! -d "/generated/magic-modules-input" ];then
+  src="/generated"
+else
+  src="/generated/magic-modules-input"
+fi
+echo $src
+cd /magic-modules; 
+for resource in $src/*; do
+  echo $resource
+  bundle exec compiler.rb -e terraform -c azure -p $resource -o /mmoutput
 done
