f9dfd9b2aac989f0beee6cd0c265617767684c50
Crystal YU <chunyu@microsoft.com>
2019-11-01 11:40:03 +0800

    remove previous generated

---
+++ run-raft.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+RESULT_FOO=`python -c 'import sys; import list_readme_path_of_all_services; import json; print list_readme_path_of_all_services.list_readme_path_of_all_services(sys.argv[1], json.loads("{\"service\":\"ALL\",\"manager\":\"resource-manager\"}"))', "$1"`
+#echo $RESULT_FOO
+for i in ${RESULT_FOO}; do
+        echo $i;
+        f="basename --$i"
+        echo $f
+        #s = $i;
+        #echo $s
+        TAGS=`python -c 'import sys; import get_tags_from_readme; print get_tags_from_readme.get_tags_from_readme(sys.argv[1])', "$i"`
+        echo $TAGS
+        for t in ${TAGS}; do
+                echo $t;
+        done
+        break
+done
+
+
+
+#!/bin/bash
+cp -rf --verbose /source/dist /autorest.cli
+cp -rf --verbose /source/input /autorest.cli
+#cd /magic-modules; bundle update --bundler; bundle
+echo $1
+whoami
+cd /cli-input-tools
+#for d in $1/*.md; do
+ # autorest --cli --use=/autorest.cli --mm --intermediate --python-sdks-folder=/generated --output-folder=/generated $di
+ # break
+#done
+RESULT_FOO=`python -c 'import sys; import list_readme_path_of_all_services; import json; print list_readme_path_of_all_services.list_readme_path_of_all_services(sys.argv[1], json.loads("{\"service\":\"ALL\",\"manager\":\"resource-manager\"}"))', "$1"`
+#echo $RESULT_FOO
+for i in ${RESULT_FOO}; do
+        echo $i;
+        #s = $i;
+        #echo $s
+        TAGS=`python -c 'import sys; import get_tags_from_readme; print get_tags_from_readme.get_tags_from_readme(sys.argv[1])', "$i"`
+        echo $TAGS
+        for t in ${TAGS}; do
+                autorest --cli --use=/autorest.cli --mm --intermediate --tag=$t --python-sdks-folder=/generated --output-folder=/generated $i
+        done
+done
+src="/generated"
+if [ ! -d "/generated/magic-modules-input" ];then
+  src="/generated"
+else
+  src="/generated/magic-modules-input"
+fi
+echo $src
+cd /magic-modules
+#gem install bundler:2.0.1; bundle update --bundler; bundle install
+#source /root/.bashrc
+export PATH=/root/.rbenv/shims:$PATH
+echo $PWD
+echo $PATH
+which rake
+for resource in $src/*; do
+  echo $resource
+  f=$(basename -- $resource)
+  echo $f
+  for pkg in $resource/*; do
+          echo $pkg
+        pname=$(basename -- $pkg)
+        outputdir="/mmoutput/$f/$pname"
+        echo $outputdir
+        makdir -p $outputdir
+        bundle exec compiler.rb -e terraform -c azure -p $pkg -o $outputdir
+  done
+done
+
+
+
+#!/bin/bash
+
+#RESULT_FOO=`python -c 'import sys; import list_readme_path_of_all_services; import json; print list_readme_path_of_all_services.list_readme_path_of_all_services(sys.argv[1], json.loads("{\"service\":\"ALL\",\"manager\":\"resource-manager\"}"))', "$1"`
+RESULT_FOO=$(python3 list_readme_path_of_all_services.py $1)
+#echo $RESULT_FOO
+for i in ${RESULT_FOO}; do
+        echo $i;
+        f="basename --$i"
+        echo $f
+        #s = $i;
+        #echo $s
+        #TAGS=`python -c 'import sys; import get_tags_from_readme; print get_tags_from_readme.get_tags_from_readme(sys.argv[1])', "$i"`
+        TAGS=$(python3 get_tags_from_readme.py $i)
+        echo $TAGS
+        for t in ${TAGS}; do
+                echo $t;
+        done
+        #break
+done
+
+
+
+#!/bin/bash
+cp -rf --verbose /source/dist /autorest.cli
+cp -rf --verbose /source/input /autorest.cli
+#cd /magic-modules; bundle update --bundler; bundle
+echo $1
+whoami
+cd /cli-input-tools
+#for d in $1/*.md; do
+ # autorest --cli --use=/autorest.cli --mm --intermediate --python-sdks-folder=/generated --output-folder=/generated $di
+ # break
+#done
+RESULT_FOO=$(python3 list_readme_path_of_all_services.py $1)
+#echo $RESULT_FOO
+for i in ${RESULT_FOO}; do
+        echo $i;
+        #s = $i;
+        #echo $s
+        #TAGS=`python -c 'import sys; import get_tags_from_readme; print get_tags_from_readme.get_tags_from_readme(sys.argv[1])', "$i"`
+        TAGS=$(python3 get_tags_from_readme.py $i)
+        echo $TAGS
+        for t in ${TAGS}; do
+                autorest --cli --use=/autorest.cli --mm --intermediate --tag=$t --python-sdks-folder=/generated --output-folder=/generated $i
+        done
+done
+src="/generated"
+if [ ! -d "/generated/magic-modules-input" ];then
+  src="/generated"
+else
+  src="/generated/magic-modules-input"
+fi
+echo $src
+cd /magic-modules
+#gem install bundler:2.0.1; bundle update --bundler; bundle install
+#source /root/.bashrc
+export PATH=/root/.rbenv/shims:$PATH
+echo $PWD
+echo $PATH
+which rake
+for resource in $src/*; do
+  echo $resource
+  f=$(basename -- $resource)
+  echo $f
+  for pkg in $resource/*; do
+          echo $pkg
+          pname=$(basename -- $pkg)
+          outputdir="/mmoutput/$f/$pname"
+          echo $outputdir
+          mkdir -p $outputdir
+          bundle exec compiler.rb -e terraform -c azure -p $pkg -o $outputdir
+  done
+done
